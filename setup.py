15ece617dc68ab109f245d96a33e24ff37b71d0b
Jieke Wu <wujieke@qiyi.com>
2014-02-26 15:57:49 +0800

    update file structure

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+setup(name='OngMani',
+      version='0.1',
+      description="As a backend of We..Chat!!",
+      author='jwu26',
+      author_email='bala@gmail.com',
+      packages=['OngMani'],
+      package_dir={'OngMani': 'src'},
+      license='BALABAL',
+     )
+      
+#py_modules=['OngMani'],
